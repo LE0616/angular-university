dc67345105092fe47adb6b32037c5699fce5f7b2
LE (Lee) <lm61613@gmail.com>
2019-03-14 16:10:45 -0500

    file config

--- angular-beginner-course/Introduction/src/app/app.component.ts
+++ angular-beginner-course/Introduction/src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-beginner-course';
+  data = {
+     title: 'angular-beginner-course'
+  };
+
+    onLogoClicked() {
+      alert('Hello World');
+    }
+
+    onKeyUp(newTitle: string) {
+      this.data.title = newTitle;
+    }
 }
+
