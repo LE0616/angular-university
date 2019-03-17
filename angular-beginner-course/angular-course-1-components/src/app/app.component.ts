8dfef627a80d2f3b3a227902e7714652bdf870a6
LE (Lee) <lm61613@gmail.com>
2019-03-17 16:42:35 -0500

    end visual layer

--- angular-beginner-course/angular-course-1-components/src/app/app.component.ts
+++ angular-beginner-course/angular-course-1-components/src/app/app.component.ts
@@ -10,6 +10,11 @@ export class AppComponent {
 
   courses = COURSES;
 
+  startDate = new Date(2000, 0, 1);
+  title = COURSES[0].description;
+  price = 9.99;
+  rate = .67;
+  course = COURSES[0];
   // coreCourse = COURSES[0];
   // rxjsCourse = COURSES[1];
   // ngrxCourse = COURSES[2];
