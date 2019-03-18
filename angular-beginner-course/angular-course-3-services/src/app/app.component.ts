747b1e57e131ba824847e4aa8ca9bdadb17e9b22
LE (Lee) <lm61613@gmail.com>
2019-03-17 21:19:53 -0500

    Angular for Beginners complete

--- angular-beginner-course/angular-course-3-services/src/app/app.component.ts
+++ angular-beginner-course/angular-course-3-services/src/app/app.component.ts
@@ -4,7 +4,8 @@ import {Course} from './model/course';
 import {CourseCardComponent} from './course-card/course-card.component';
 import {HighlightedDirective} from './directives/highlighted.directive';
 import {Observable} from 'rxjs';
-import { HttpClient, HttpParams } from '@angular/common/http';
+// import { HttpClient, HttpParams } from '@angular/common/http';
+import {CoursesService} from './services/courses.service';
 
 @Component({
   selector: 'app-root',
@@ -13,24 +14,27 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
 
-  courses;
+  courses$ : Observable<Course[]>;
+  // courses;
   // courses = COURSES;
 
-  constructor(private http: HttpClient) {
+  constructor(private coursesService: CoursesService) {
 
   }
 
   ngOnInit() {
 
-    const params = new HttpParams()
-    .set("page", "1")
-    .set("pageSize", "10");
+    // this.http.get("/api/courses", {params}).subscribe(
+    //   courses => this.courses = courses
+    // );
 
-    this.http.get("/api/courses", {params}).subscribe(
-      courses => this.courses = courses
-    );
+    this.courses$ = this.coursesService.loadCourses();
   }
 
-
+  save(course:Course) {
+    this.coursesService.saveCourse(course).subscribe(
+      () => console.log('Course Saved!')
+    );
+  }
 
 }
